a47e3f103aaf5bdfd2a20ca49ca47d6510de8c90
Davin <lampdavin@gmail.com>
2024-04-26 01:18:28 -0400

    Kinda works?
    Issue seems to be that it keeps reading the same value over and over,
    but it's stale - the sender sends good info, but no matter how many
    times the reader reads, it never actually propagates (until, for some
    reason, something else also reads it. In this case, the test bench)

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ enum Error {
     Decode,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 struct Encoded {
     t: u16,
 }
@@ -21,6 +21,7 @@ unsafe impl Send for Receiver {}
 struct Receiver {
     reg: *mut u16,
     level: bool,
+    counter: usize,
 }
 
 impl Sender {
@@ -30,8 +31,13 @@ impl Sender {
             perceived = ptr::read_volatile(self.reg);
         }
 
+        // if self.counter > 1_000 {
+        //     panic!("Sender reg: {perceived:0b}")
+        // }
+
         // println!("{perceived:0b}");
         if (!self.level && perceived != 0) || (self.level && perceived != u16::MAX) {
+            // self.counter += 1;
             return Err(Error::Blocked);
         }
 
@@ -43,16 +49,17 @@ impl Sender {
             ptr::write_volatile(self.reg, enc.t);
         }
 
-        loop {
-            let perceived;
-            unsafe {
-                perceived = ptr::read_volatile(self.reg);
-            }
-            if perceived == enc.t {
-                break;
-            }
-        }
+        // loop {
+        //     let perceived;
+        //     unsafe {
+        //         perceived = ptr::read_volatile(self.reg);
+        //     }
+        //     if perceived == enc.t {
+        //         break;
+        //     }
+        // }
 
+        // self.counter = 0;
         self.level = !self.level;
         Ok(())
     }
@@ -94,15 +101,16 @@ impl Receiver {
                     ptr::write_volatile(self.reg, ack);
                 }
 
-                loop {
-                    let perceived;
-                    unsafe {
-                        perceived = ptr::read_volatile(self.reg);
-                    }
-                    if perceived == ack {
-                        break;
-                    }
-                }
+                // loop {
+                //     let perceived;
+                //     unsafe {
+                //         perceived = ptr::read_volatile(self.reg);
+                //     }
+
+                //     if perceived == ack {
+                //         break;
+                //     }
+                // }
 
                 self.level = !self.level;
                 Ok(t)
@@ -128,6 +136,20 @@ impl Receiver {
     }
 }
 
+fn dbg_encode(t: u8) -> Encoded {
+    let mut t_encoded: u16 = 0;
+    for i in 0..8 {
+        let bit = (t >> i) & 0b1;
+        match bit {
+            0b0 => t_encoded |= 0b10 << (2 * i),
+            0b1 => t_encoded |= 0b01 << (2 * i),
+            _ => unreachable!(),
+        }
+    }
+
+    Encoded { t: t_encoded }
+}
+
 // channel implies a memory leak of its internal
 // boxed_reg. It's up to the user to deallocate it
 // properly.
@@ -143,6 +165,7 @@ unsafe fn channel() -> (Sender, Receiver) {
         Receiver {
             reg: reg_ptr,
             level: false,
+            counter: 0,
         },
     )
 }
@@ -218,18 +241,42 @@ mod tests {
         let data = vec![0xA5, 0xF1, 0x23, 0x00];
         let c_data = data.clone();
 
-        let range = 2_000_000;
+        let range = 10_000_000;
+
+        let range = 1_000_000;
+        let mut counter = 0;
 
         let handle = thread::spawn(move || {
             for _ in 0..range {
                 for datum in c_data.iter() {
                     loop {
+                        if counter > range {
+                            let mut perceived = vec![];
+                            unsafe {
+                                perceived.push(ptr::read_volatile(rx.reg));
+                                perceived.push(ptr::read_volatile(rx.reg));
+                                perceived.push(ptr::read_volatile(rx.reg));
+                                perceived.push(ptr::read_volatile(rx.reg));
+                            }
+                            panic!(
+                                "Encoded datum: {:016b}\nReceiver perceived[0]: {:016b}\nReceiver perceived[1]: {:016b}\nReceiver perceived[2]: {:016b}\nReceiver perceived[3]: {:016b}\n",
+                                dbg_encode(*datum).t,
+                                perceived[0],
+                                perceived[1],
+                                perceived[2],
+                                perceived[3],
+                            );
+                        }
+
                         match rx.try_recv() {
                             Ok(d) => {
                                 assert_eq!(d, *datum);
                                 break;
                             }
-                            Err(_) => continue,
+                            Err(_) => {
+                                counter += 1;
+                                continue;
+                            }
                         }
                     }
                 }
