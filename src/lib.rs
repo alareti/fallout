2b846752a5330b1d382b3683de1d64e2c143968f
Davin <lampdavin@gmail.com>
2024-04-26 01:45:09 -0400

    NOW IT ACTUALLY WORKS!!!
    :)

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,6 @@ unsafe impl Send for Receiver {}
 struct Receiver {
     reg: *mut u16,
     level: bool,
-    counter: usize,
 }
 
 impl Sender {
@@ -31,13 +30,7 @@ impl Sender {
             perceived = ptr::read_volatile(self.reg);
         }
 
-        // if self.counter > 1_000 {
-        //     panic!("Sender reg: {perceived:0b}")
-        // }
-
-        // println!("{perceived:0b}");
         if (!self.level && perceived != 0) || (self.level && perceived != u16::MAX) {
-            // self.counter += 1;
             return Err(Error::Blocked);
         }
 
@@ -49,17 +42,6 @@ impl Sender {
             ptr::write_volatile(self.reg, enc.t);
         }
 
-        // loop {
-        //     let perceived;
-        //     unsafe {
-        //         perceived = ptr::read_volatile(self.reg);
-        //     }
-        //     if perceived == enc.t {
-        //         break;
-        //     }
-        // }
-
-        // self.counter = 0;
         self.level = !self.level;
         Ok(())
     }
@@ -100,17 +82,6 @@ impl Receiver {
                 unsafe {
                     ptr::write_volatile(self.reg, ack);
                 }
-
-                // loop {
-                //     let perceived;
-                //     unsafe {
-                //         perceived = ptr::read_volatile(self.reg);
-                //     }
-
-                //     if perceived == ack {
-                //         break;
-                //     }
-                // }
 
                 self.level = !self.level;
                 Ok(t)
@@ -165,7 +136,6 @@ unsafe fn channel() -> (Sender, Receiver) {
         Receiver {
             reg: reg_ptr,
             level: false,
-            counter: 0,
         },
     )
 }
@@ -173,6 +143,48 @@ unsafe fn channel() -> (Sender, Receiver) {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn thread_transfer() {
+        use std::thread;
+
+        let (mut tx, mut rx) = unsafe { channel() };
+
+        let data = vec![0xA5, 0xF1, 0x23, 0x00];
+        let c_data = data.clone();
+
+        let range = 100_000_000;
+        let handle = thread::spawn(move || {
+            for _ in 0..range {
+                for datum in c_data.iter() {
+                    loop {
+                        match rx.try_recv() {
+                            Ok(d) => {
+                                assert_eq!(d, *datum);
+                                break;
+                            }
+                            Err(_) => {
+                                continue;
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        for _ in 0..range {
+            for datum in data.iter() {
+                loop {
+                    match tx.try_send(tx.encode(*datum)) {
+                        Ok(_) => break,
+                        Err(_) => continue,
+                    }
+                }
+            }
+        }
+
+        handle.join().unwrap();
+    }
 
     // #[test]
     // fn simple_transfer() {
@@ -231,69 +243,4 @@ mod tests {
     //         }
     //     }
     // }
-
-    #[test]
-    fn thread_transfer() {
-        use std::thread;
-
-        let (mut tx, mut rx) = unsafe { channel() };
-
-        let data = vec![0xA5, 0xF1, 0x23, 0x00];
-        let c_data = data.clone();
-
-        let range = 10_000_000;
-
-        let range = 1_000_000;
-        let mut counter = 0;
-
-        let handle = thread::spawn(move || {
-            for _ in 0..range {
-                for datum in c_data.iter() {
-                    loop {
-                        if counter > range {
-                            let mut perceived = vec![];
-                            unsafe {
-                                perceived.push(ptr::read_volatile(rx.reg));
-                                perceived.push(ptr::read_volatile(rx.reg));
-                                perceived.push(ptr::read_volatile(rx.reg));
-                                perceived.push(ptr::read_volatile(rx.reg));
-                            }
-                            panic!(
-                                "Encoded datum: {:016b}\nReceiver perceived[0]: {:016b}\nReceiver perceived[1]: {:016b}\nReceiver perceived[2]: {:016b}\nReceiver perceived[3]: {:016b}\n",
-                                dbg_encode(*datum).t,
-                                perceived[0],
-                                perceived[1],
-                                perceived[2],
-                                perceived[3],
-                            );
-                        }
-
-                        match rx.try_recv() {
-                            Ok(d) => {
-                                assert_eq!(d, *datum);
-                                break;
-                            }
-                            Err(_) => {
-                                counter += 1;
-                                continue;
-                            }
-                        }
-                    }
-                }
-            }
-        });
-
-        for _ in 0..range {
-            for datum in data.iter() {
-                loop {
-                    match tx.try_send(tx.encode(*datum)) {
-                        Ok(_) => break,
-                        Err(_) => continue,
-                    }
-                }
-            }
-        }
-
-        handle.join().unwrap();
-    }
 }
